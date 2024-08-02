4a75ddddd79073f0c53162007e6d4e638156a8b3
alexisljn <alexis.lejeune@gmx.fr>
2024-08-02 16:58:45 +0200

    Wrap Server Actions functions by try catch statement

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,25 +6,25 @@ import { Client } from 'pg';
 import { z } from 'zod';
 
 async function connectToDatabase() {
-    const client = new Client({
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      host: process.env.POSTGRES_HOST,
-      port: 5432,
-      database: process.env.POSTGRES_DATABASE
-    })
-  
-    await client.connect()
-  
-    return client;
-  }
+	const client = new Client({
+		user: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASSWORD,
+		host: process.env.POSTGRES_HOST,
+		port: 5432,
+		database: process.env.POSTGRES_DATABASE
+	})
+
+	await client.connect()
+
+	return client;
+}
 
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
-    date: z.string(),
+	id: z.string(),
+	customerId: z.string(),
+	amount: z.coerce.number(),
+	status: z.enum(['pending', 'paid']),
+	date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
@@ -33,58 +33,70 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 
 export async function createInvoice(formData: FormData) {
-    const {customerId, amount, status} = CreateInvoice.parse(Object.fromEntries(formData));
+	try {
+		const { customerId, amount, status } = CreateInvoice.parse(Object.fromEntries(formData));
 
-    const amountInCents = amount * 100;
+		const amountInCents = amount * 100;
 
-    const date = new Date().toISOString().split('T')[0];
+		const date = new Date().toISOString().split('T')[0];
 
-    const client = await connectToDatabase();
+		const client = await connectToDatabase();
 
-    await client.query(`INSERT INTO invoices (customer_id, amount, status, date) VALUES ($1, $2, $3, $4)`, [customerId, amountInCents, status, date]);
+		await client.query(`INSERT INTO invoices (customer_id, amount, status, date) VALUES ($1, $2, $3, $4)`, [customerId, amountInCents, status, date]);
 
-    await client.end();
+		await client.end();
+	} catch (error: any) {
+		console.error(error);
+	}
 
-   revalidatePath('/dashboard/invoices'); // Related to current user (meaning we're mutating his client side router cache)
+	revalidatePath('/dashboard/invoices'); // Related to current user (meaning we're mutating his client side router cache)
 
-   redirect('/dashboard/invoices');
+	redirect('/dashboard/invoices');
 }
 
- 
+
 export async function updateInvoice(id: string, formData: FormData) {
-    // Get values one by one from FormData
-    const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
+	try {
+		// Get values one by one from FormData
+		const { customerId, amount, status } = UpdateInvoice.parse({
+			customerId: formData.get('customerId'),
+			amount: formData.get('amount'),
+			status: formData.get('status'),
+		});
 
-  const amountInCents = amount * 100;
+		const amountInCents = amount * 100;
 
-  const client = await connectToDatabase();
+		const client = await connectToDatabase();
 
-  await client.query(`
-    UPDATE invoices
-    SET customer_id = $1, amount = $2, status = $3
-    WHERE id = $4
-  `, [customerId, amountInCents, status, id]);
-  
-  await client.end();
+		await client.query(`
+			UPDATE invoices
+			SET customer_id = $1, amount = $2, status = $3
+			WHERE id = $4
+		`, [customerId, amountInCents, status, id]);
 
-  revalidatePath('/dashboard/invoices');
+		await client.end();
+	} catch (error: any) {
+		console.error(error);
+	}
+	
+	revalidatePath('/dashboard/invoices');
 
-  redirect('/dashboard/invoices');
+	redirect('/dashboard/invoices');
 }
 
 /* Since this action is being called in the /dashboard/invoices path, we don't need to call redirect. 
  * Calling revalidatePath will trigger a new server request and re-render the table.
  */
 export async function deleteInvoice(id: string) {
-    const client = await connectToDatabase();
+	try {
+		const client = await connectToDatabase();
 
-    await client.query(`DELETE FROM invoices WHERE id = $1`, [id]);
-    
-    await client.end();
+		await client.query(`DELETE FROM invoices WHERE id = $1`, [id]);
 
-    revalidatePath('/dashboard/invoices');
-  }
+		await client.end();
+
+		revalidatePath('/dashboard/invoices');	
+	} catch (error: any) {
+		console.error(error);
+	}
+}
